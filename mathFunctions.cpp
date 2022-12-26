32b46af0e0823237cc5d8fee089eeb6b5214bfad
Aleksey <alekseygisich@yandex.by>
2022-12-26 12:28:57 +0300

    change

--- mathFunctions.cpp
+++ mathFunctions.cpp
@@ -8,4 +8,7 @@ long long sum(long long first, long long second) {
 long long multiplication(long long first, long long second) {
 	return first * second;
 };
+long long subtraction(long long first, long long second) {
+	return first - second;
+};
 
