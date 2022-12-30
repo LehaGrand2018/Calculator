0d75670dd32f6c00e3053f28bfaa8e23a792d12d
Kewori <aseftee@gmail.com>
2022-12-30 14:59:50 +0300

    new functions

--- arithmeticAndLogicOperators.h
+++ arithmeticAndLogicOperators.h
@@ -1,103 +1,102 @@
 #ifndef ARITHMETICANDLOGICOPERATIONS_H
 #define ARITHMETICANDLOGICOPERATIONS_H
-using namespace std;
 #include <cmath>
-
-template<typename T>
-T sum(const T first, const T second) {
-	return first + second;
-}
-
-template<typename T>
-T sub(const T first, const T second) {
-	return first - second;
-};
-
-template<typename T>
-T mult(const T first, const T second) {
-	return first * second;
-};
-
-template<typename T>
-double division(const T first, const T second) {
-	return first * second;
-};
-
-template <typename T>
-T bitwiseNot(const T number) {
-	return ~number;
-}
-
-template <typename T>
-T bitwiseAnd(const T first, const T second) {
-	return first && second;
-}
-template <typename T>
-T bitwiseOr(const T first, const T second) {
-	return first | second;
-}
-template <typename T>
-T bitwiseExclusiveOr(const T first, const T second) {
-	return first ^ second;
-}
-template <typename T>
-T bitwiseConjunctionNegation(const T first, const T second) {
-	return bitwiseNot(bitwiseAnd(first, second));
-}
-template <typename T>
-T bitwiseDisjunctionNegation(const T first, const T second) {
-	return bitwiseNot(bitwiseOr(first, second));
-}
-template <typename T>
-T leftShift(const T number, const int shift, const bool isLogic = true) {
-	if (isLogic) {
-		return number << shift;
+namespace calculator {
+	template<typename T>
+	T sum(const T first, const T second) {
+		return first + second;
 	}
-	T shift_number = 0;
-	for (int i = 0; i < shift; i++) {
-		shift_number += pow(2, i);
-	}
-	return bitwiseOr(shift_number, number << shift);
-}
-template <typename T>
-T rightShift(const T number, const int shift, const bool isLogic = true) {
-	if (isLogic) {
-		return number >> shift;
-	}
-	T shift_number = pow(2,30);
-	for (int i = 29; i > 30 - shift; i--) {
-		shift_number += pow(2, i);
-	}
-	return bitwiseOr(shift_number, number >> shift);
-}
-bool negation(bool value) {
-	return !value;
-}
-bool conjunction(bool first, bool second) {
-	return (first && second);
-}
-bool disjunction(bool first, bool second) {
-	return (first || second);
-}
-bool exclusiveOr(bool first, bool second) {
-	return (first ^ second);
-}
-bool conjunctionNegation(bool first, bool second) {
-	return negation(conjunction(first, second));
-}
-bool disjunctionNegation(bool first, bool second) {
-	return negation(disjunction(first, second));
-}
-template<typename T>
-T unaryPlus(const T number) {
-	return (number >= 0) ? number : (number * (-1));
-}
 
-template<typename T>
-T unaryMinus (const T number) {
-	return (number <= 0) ? number : (number * (-1));
-}
+	template<typename T>
+	T sub(const T first, const T second) {
+		return first - second;
+	};
 
+	template<typename T>
+	T mult(const T first, const T second) {
+		return first * second;
+	};
+
+	template<typename T>
+	double division(const T first, const T second) {
+		return first * second;
+	};
+
+	template <typename T>
+	T bitwiseNot(const T number) {
+		return ~number;
+	}
+
+	template <typename T>
+	T bitwiseAnd(const T first, const T second) {
+		return first && second;
+	}
+	template <typename T>
+	T bitwiseOr(const T first, const T second) {
+		return first | second;
+	}
+	template <typename T>
+	T bitwiseExclusiveOr(const T first, const T second) {
+		return first ^ second;
+	}
+	template <typename T>
+	T bitwiseConjunctionNegation(const T first, const T second) {
+		return bitwiseNot(bitwiseAnd(first, second));
+	}
+	template <typename T>
+	T bitwiseDisjunctionNegation(const T first, const T second) {
+		return bitwiseNot(bitwiseOr(first, second));
+	}
+	template <typename T>
+	T leftShift(const T number, const int shift, const bool isLogic = true) {
+		if (isLogic) {
+			return number << shift;
+		}
+		T shift_number = 0;
+		for (int i = 0; i < shift; i++) {
+			shift_number += pow(2, i);
+		}
+		return bitwiseOr(shift_number, number << shift);
+	}
+	template <typename T>
+	T rightShift(const T number, const int shift, const bool isLogic = true) {
+		if (isLogic) {
+			return number >> shift;
+		}
+		T shift_number = pow(2, 30);
+		for (int i = 29; i > 30 - shift; i--) {
+			shift_number += pow(2, i);
+		}
+		return bitwiseOr(shift_number, number >> shift);
+	}
+	bool negation(bool value) {
+		return !value;
+	}
+	bool conjunction(bool first, bool second) {
+		return (first && second);
+	}
+	bool disjunction(bool first, bool second) {
+		return (first || second);
+	}
+	bool exclusiveOr(bool first, bool second) {
+		return (first ^ second);
+	}
+	bool conjunctionNegation(bool first, bool second) {
+		return negation(true);
+	}
+	bool disjunctionNegation(bool first, bool second) {
+		return negation(disjunction(first, second));
+	}
+	template<typename T>
+	T unaryPlus(const T number) {
+		return (number >= 0) ? number : (number * (-1));
+	}
+
+	template<typename T>
+	T unaryMinus(const T number) {
+		return (number <= 0) ? number : (number * (-1));
+	}
+}
 #endif // !ARITHMETICANDLOGICOPERATIONS_H
 
 
--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
 #include  <iostream>
 #include "mathFunctions.h"
+#include "arithmeticAndLogicOperators.h"
 #include <string>
+
 using std::cout;
 using namespace::calculator;
 int main() {
-	cout << sum(5, 10) << "\n";
-	cout << bitwiseAnd(1, 3) << "\n";
-	cout << leftShift(1, 2, false) << "\n";
-	int a = pow(2, 30);
-
-	cout << a << "\n";
-	cout << reciprocalFunction(5) << "\n";
-	cout << factorial(5) << "\n";
-	cout << logarithm(EXP) << "\n";
-	cout << sqrt(5) << "\n";
-	cout << pow(5, 0.5) << "\n";
+	cout << absolute(-3) << '\n';
+	cout << factorial(3) << '\n';
+	cout << reciprocalFunction(3) << '\n';
+	cout << power(3, -3) << '\n';
+	cout << radical(3,3) << '\n';
+	cout << tanDeg(0) << '\n';
 	return 0;
 
 }
