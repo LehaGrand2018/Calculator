74568df511ef242ceb99f31d28ef71ee0514e82f
Aleksey <alekseygisich@yandex.by>
2023-01-02 17:27:31 +0300

    .

--- Program.h
+++ Program.h
@@ -34,22 +34,22 @@ public:
 			cout << "Неправильный вариант ответа. Попробуйте еще раз!\n";
 			break;
 		}*/
-		cout << cosDeg(-60);
+		/*cout << cosDeg(-60);*/
 		return 0;
 		
 
 
 
 
-		//cout << absolute(-3) << '\n';
-		//cout << factorial(3) << '\n';
-		//cout << reciprocalFunction(3) << '\n';
-		//cout << power(3, -3) << '\n';
-		//cout << radical(3, 3) << '\n';
-		//cout << cotDeg(30) << '\n';
-		//string a = reverse_polish_notation("3+3/3");
-		//cout << count_reverse_polish_notation(a);
-		//return 0;
+		cout << absolute(-3) << '\n';
+		cout << factorial(3) << '\n';
+		cout << reciprocalFunction(3) << '\n';
+		cout << power(3, -3) << '\n';
+		cout << radical(3, 3) << '\n';
+		cout << cotDeg(30) << '\n';
+		string a = reverse_polish_notation("3+3/3");
+		cout << count_reverse_polish_notation(a);
+		return 0;
 	}
 
 };
